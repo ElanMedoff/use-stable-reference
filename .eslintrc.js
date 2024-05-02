3d8b782b9bb93b832b307fc7be52e1cf150014fa
Elan <54599408+ElanMedoff@users.noreply.github.com>
2024-05-01 23:16:25 -0400

    chore: add eslint, prettier, typescript, tsup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+/** @type { import("eslint").Linter.Config } */
+const config = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: [
+    "**/.eslintrc.js",
+    "**/tsup.config.ts",
+    "**/jest.config.js",
+    "**/.prettierrc.js",
+  ],
+  rules: {
+    "@typescript-eslint/unbound-method": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
+
+module.exports = config;
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { hello } from "./index";
+
+it("test", () => {
+  expect(hello).toBe("hello, world!");
+});
