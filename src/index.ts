0236cc10433381af26422fb58957800340e1dbbc
Elan <54599408+ElanMedoff@users.noreply.github.com>
2024-05-06 23:30:41 -0400

    feat: add intellisence comments

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import React from "react";
 
+/**
+ * @param `val` The value to be "stabilized".
+ * @returns A referentially stable callback that can be used to access the latest version of the value.
+ */
 function useStableValue<Val>(val: Val) {
   const valRef = React.useRef(val);
   valRef.current = val;
   return React.useCallback(() => valRef.current, []);
 }
 
+/**
+ * @param `cb` The callback to be "stabilized".
+ * @returns An up-to-date referentially stable callback.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function useStableCallback<Cb extends (...args: any[]) => any>(cb: Cb): Cb {
   const cbRef = React.useRef(cb);
